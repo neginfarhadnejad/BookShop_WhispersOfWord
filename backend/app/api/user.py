94a7d68dbb1401a115410fb46c465009446661fe
neginfarhadnejad <negin.farhadnejad@gmail.com>
2025-04-27 00:07:27 +0330

    Add user registration with password hashing and email duplication check

---
+++ backend/app/api/user.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Depends, HTTPException ,status
+from sqlalchemy.orm import Session
+from app.schemas.user import UserCreate
+from app.db.database import SessionLocal
+import bcrypt
+from app.models.user import User 
+
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def hash_password(password: str) -> str:
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed.decode('utf-8')
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    # چک کن ایمیل وجود داره یا نه
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+    
+    hashed_password = hash_password(user.password)
+    
+    db_user = User(
+        name=user.name,
+        email=user.email,
+        password=hashed_password
+    )
+    
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return {"message": "User registered successfully", "user_id": db_user.id}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from app.db.database import engine
+from app.api import user as user_route   
+from app.models import user as user_model
+
 
 app = FastAPI()
+
+user_model.Base.metadata.create_all(bind=engine)
+app.include_router(user_route.router)
+
 
 @app.get("/")
 def root():
