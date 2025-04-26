6206d656023666b24c70a475f2f8fd8e591c654f
neginfarhadnejad <negin.farhadnejad@gmail.com>
2025-04-27 00:36:18 +0330

    Add password validation (length, uppercase, lowercase, number, special character)

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr ,validator
+import re
 
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
+
+    @validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Password must contain at least one number")
+        if not re.search(r"[!@#$%^&*()_+{}\[\]:;\"'<>,.?/~`\\|-]", value):
+            raise ValueError("Password must contain at least one special character (!@#$%^&...)")
+        return value
